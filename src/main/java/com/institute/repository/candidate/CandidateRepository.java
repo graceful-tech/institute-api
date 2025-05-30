887038f45357b8def9c2304ed34b7eaca2e3a6b5
Sri keerthivasan <srikeerthivasan111@gmail.com>
2025-05-30 16:28:03 +0530

    candidate service added

--- src/main/java/com/institute/repository/candidate/CandidateRepository.java
+++ src/main/java/com/institute/repository/candidate/CandidateRepository.java
@@ -1,6 +1,10 @@
 package com.institute.repository.candidate;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.institute.entity.candidate.CandidateEntity;
@@ -10,5 +14,11 @@ import jakarta.transaction.Transactional;
 @Repository
 @Transactional
 public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
+
+	@Query(value = "select * from candidates where mobile_number  = :mobileNumber ", nativeQuery = true)
+	CandidateEntity getCandidateDetailsByMobileNumber(@Param("mobileNumber") String mobileNumber);
+
+	@Query(value = "select * from candidates where created_username = :createdUserName ", nativeQuery = true)
+	CandidateEntity getCandidateByUserName(@Param("createdUserName") String createdUserName);
 
 }
--- src/main/java/com/institute/service/impl/candidate/CandidateServiceImpl.java
+++ src/main/java/com/institute/service/impl/candidate/CandidateServiceImpl.java
@@ -1,5 +1,70 @@
 package com.institute.service.impl.candidate;
 
-public class CandidateServiceImpl {
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.institute.dto.candidate.CandidateDto;
+import com.institute.entity.candidate.CandidateEntity;
+import com.institute.repository.candidate.CandidateRepository;
+import com.institute.service.candidate.CandidateService;
+
+@Service
+public class CandidateServiceImpl implements CandidateService {
+
+	private static final Logger logger = LoggerFactory.getLogger(CandidateServiceImpl.class);
+
+	@Autowired
+	CandidateRepository candidatesRepository;
+
+	@Autowired
+	ModelMapper modelMapper;
+
+	@Override
+	public Long createCandidate(CandidateDto candiateDto, String username) {
+		logger.debug("Service :: createCandidate :: Entered");
+
+		CandidateEntity candidateEntity = null;
+		CandidateEntity saveCandiate = null;
+		Long id = null;
+
+		try {
+
+			candidateEntity = candidatesRepository.getCandidateDetailsByMobileNumber(candiateDto.getMobileNumber());
+
+			if (Objects.nonNull(candidateEntity)) {
+
+				candidateEntity = modelMapper.map(candiateDto, CandidateEntity.class);
+				candidateEntity.setId(candidateEntity.getId());
+				candidateEntity.setModifiedDate(LocalDateTime.now());
+				candidateEntity.setModifiedUser(candiateDto.getCreatedUser());
+				candidateEntity.setCreatedUserName(username);
+
+				saveCandiate = candidatesRepository.save(candidateEntity);
+
+				id = saveCandiate.getId();
+			} else {
+				candidateEntity = modelMapper.map(candiateDto, CandidateEntity.class);
+				candidateEntity.setCreatedDate(LocalDateTime.now());
+				candidateEntity.setCreatedUser(candiateDto.getCreatedUser());
+				candidateEntity.setCreatedUserName(username);
+
+				saveCandiate = candidatesRepository.save(candidateEntity);
+
+				id = saveCandiate.getId();
+			}
+
+		} catch (Exception e) {
+			logger.error("Service :: createCandidate :: Exception :: " + e.getMessage());
+		}
+		logger.debug("Service :: createCandidate :: Exited");
+
+		return id;
+	}
 
 }
